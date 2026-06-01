a3d632c1e7eb8ae198e45272dd98132f26f61d6c
kesther04 <ogbukesther@gmail.com>
2026-06-01 10:42:58 +0100

    seo optimization  and image loading updated

--- src/pages/Waitlist.tsx
+++ src/pages/Waitlist.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { motion } from "framer-motion"
-import logo2 from "../assets/images/logo2.png"
+// import logo2 from "../assets/images/logo2.png"
 import { supabase } from "../lib/supabase"
 import { useSearchParams } from "react-router-dom"
 
@@ -77,7 +77,8 @@ export default function Waitlist() {
                 transition={{ duration: 0.5, ease: "easeOut" }}
             >
                 <motion.img
-                    src={logo2}
+                    // src={logo2}
+                    src="https://res.cloudinary.com/dw0y0pik4/image/upload/logo2_pbqb3y"
                     alt="logo"
                     className="rounded-full w-32 py-2"
                     initial={{ opacity: 0, x: -16 }}
